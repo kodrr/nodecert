6792dabe59d341cfa205affff84c37f9dfe9e4c7
kodrr <ntlauigan@gmail.com>
2023-07-30 22:42:26 +0800

    Add labs

--- chapter-15-child-process/childStdio2.js
+++ chapter-15-child-process/childStdio2.js
@@ -1,9 +1,19 @@
 "use strict";
-const { spawn } = require("child_process");
-const sp = spawn(
+// const { spawn } = require("child_process");
+// const sp = spawn(
+//   process.execPath,
+//   ["-e", `console.error('err output'); process.stdin.pipe(process.stdout)`],
+//   { stdio: ["pipe", "inherit", "ignore"] }
+// );
+// sp.stdin.write("this input will become output\n");
+// sp.stdin.end();
+
+const { spawnSync } = require("child_process");
+spawnSync(
   process.execPath,
   ["-e", `console.error('err output'); process.stdin.pipe(process.stdout)`],
-  { stdio: ["pipe", "inherit", "ignore"] }
+  {
+    input: "this input will become output\n",
+    stdio: ["pipe", "inherit", "ignore"],
+  }
 );
-sp.stdin.write("this input will become output\n");
-sp.stdin.end();
