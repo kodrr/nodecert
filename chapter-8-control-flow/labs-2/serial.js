99414343558b765545d41cf6ae093df28efe1607
kodrr <ntlauigan@gmail.com>
2023-08-11 07:41:52 +0800

    Remove duplicate files

--- chapter-8-control-flow/labs-2/serial.js
+++ chapter-8-control-flow/labs-2/serial.js
@@ -1,25 +1,42 @@
-'use strict'
-const { promisify } = require('util')
+"use strict";
+const { promisify } = require("util");
 
-const print = (err, contents) => { 
-  if (err) console.error(err)
-  else console.log(contents) 
-}
+const print = (err, contents) => {
+  if (err) console.error(err);
+  else console.log(contents);
+};
 
 const opA = (cb) => {
   setTimeout(() => {
-    cb(null, 'A')
-  }, 500)
-}
+    cb(null, "A");
+  }, 500);
+};
 
 const opB = (cb) => {
   setTimeout(() => {
-    cb(null, 'B')
-  }, 250)
-}
+    cb(null, "B");
+  }, 250);
+};
 
 const opC = (cb) => {
   setTimeout(() => {
-    cb(null, 'C')
-  }, 125)
-}
+    cb(null, "C");
+  }, 125);
+};
+
+const aPromisified = promisify(opA);
+const bPromisified = promisify(opB);
+const cPromisified = promisify(opC);
+
+aPromisified()
+  .then((a) => {
+    print(a);
+    return bPromisified();
+  })
+  .then((b) => {
+    print(b);
+    return cPromisified();
+  })
+  .then((c) => {
+    print(c);
+  });
