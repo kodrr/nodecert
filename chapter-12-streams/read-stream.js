96ed6d204939eecba54f68c467bd604269e94a2a
kodrr <ntlauigan@gmail.com>
2023-08-13 09:21:34 +0800

    Edit readable-stream

--- chapter-12-streams/read-stream.js
+++ chapter-12-streams/read-stream.js
@@ -1,5 +1,4 @@
-'use strict'
-
+"use strict";
 // const fs = require('fs')
 // const readable = fs.createReadStream(__filename)
 
@@ -8,17 +7,34 @@
 // })
 // readable.on('end', ()=>{console.log('finish reading')})
 
-const { Readable } = require('stream')
+const { Readable } = require("stream");
 const createReadStream = () => {
- const data = ['some', 'data', 'to', 'read']
- return new Readable({
-read () {
-if (data.length === 0) this.push(null)
-else this.push(data.shift())
-}
- })
-}
+  const data = ["some", "data", "to", "read"];
+  return new Readable({
+    //objectMode: true, if this is not use default mode is binary meaning buffer
+    //encoding here will turn string to buffer to string
+    encoding: "utf-8",
+    read() {
+      if (data.length === 0) this.push(null);
+      else this.push(data.shift());
+    },
+  });
+};
 
-const readable = createReadStream()
-readable.on('data', (data) => { console.log('got data', data) })
-readable.on('end', () => { console.log('finished reading') })
+const readable = createReadStream();
+readable.on("data", (data) => {
+  console.log("got data", data);
+});
+readable.on("end", () => {
+  console.log("finished reading");
+});
+
+//iterable structure
+// const { Readable } = require("stream");
+// const readable = Readable.from(["some", "data", "to", "read"]);
+// readable.on("data", (data) => {
+//   console.log("got data", data);
+// });
+// readable.on("end", () => {
+//   console.log("finished reading");
+// });
