bf7394bda0fe88ae50a1db0d2d53a2396b6c7aa5
kodrr <ntlauigan@gmail.com>
2023-08-26 01:08:05 +0800

    Edit chapter-10 error handling

---
+++ chapter-10-Error-handling/dont-do-this.js
@@ -0,0 +1,58 @@
+class OddError extends Error {
+ constructor (varName = '') {
+    //error message = varName + ' must be even'
+super(varName + ' must be even')
+
+ }
+ get name () { return 'OddError' }
+}
+
+function codify(error, code){
+   error.code = code
+   return error;
+}
+
+function doTask (amount) {
+    if (typeof amount !== 'number') throw codify(new TypeError('amount must be a number'), 'ERR_AMOUNT_MUST_BE_NUMBER')
+    if (amount <= 0) throw codify(new RangeError('amount must be greater than zero'), 'ERR_AMOUNT_MUST_EXCEED_ZERO')
+    if (amount % 2) throw codify(new OddError('amount'), 'ERR_MUST_BE_EVEN')
+    return amount / 2
+   }
+
+try {
+    //it will not be handle in the catch because it will run after 100ms
+setTimeout(() => {
+   const result = doTask(3)
+   console.log('result', result)
+    }, 100)
+} catch (err) {
+    if (err.code === 'ERR_AMOUNT_MUST_BE_NUMBER') {
+   console.error('wrong type')
+    } else if (err.code === 'ERRO_AMOUNT_MUST_EXCEED_ZERO') {
+   console.error('out of range')
+    } else if (err.code === 'ERR_MUST_BE_EVEN') {
+   console.error('cannot be odd')
+    } else { 
+   console.error('Unknown error', err)
+    }
+}
+
+//do below instead
+// setTimeout(()=>{
+//   try {
+//    const result = doTask(3)
+//    console.log('result', result)
+// } catch (err) {
+//     if (err.code === 'ERR_AMOUNT_MUST_BE_NUMBER') {
+//    console.error('wrong type')
+//     } else if (err.code === 'ERRO_AMOUNT_MUST_EXCEED_ZERO') {
+//    console.error('out of range')
+//     } else if (err.code === 'ERR_MUST_BE_EVEN') {
+//    console.error('cannot be odd')
+//     } else { 
+//    console.error('Unknown error', err)
+//     }
+// }  
+// }, 1000)
+
+
--- chapter-10-Error-handling/native-errors.js
+++ chapter-10-Error-handling/native-errors.js
@@ -1,5 +1,13 @@
+class OddError extends Error {
+ constructor (varName = '') {
+    //error message = varName + ' must be even'
+super(varName + ' must be even')
+this.code = 'ERR_MUST_BE_EVEN'
+ }
+ get name () { return 'OddError' }
+}
+
 function doTask (amount) {
-    console.log('this is amount', amount)
     if (typeof amount !== 'number') throw new TypeError('amount must be a number')
     if (amount <= 0) throw new RangeError('amount must be greater than zero')
     if (amount % 2) throw new OddError('amount')
@@ -8,15 +16,16 @@ function doTask (amount) {
 
 try { 
     const result = doTask(4)
-    result()
+    //result() //this will throw a TypeError
     console.log('result', result)
    } catch (err) {
+    console.log(err)
         if (err instanceof TypeError) {
         console.error('wrong type')
         } else if (err instanceof RangeError) {
         console.error('out of range')
-        } else if (err.code === 'ERR_MUST_BE_EVEN') {
-        console.error('cannot be odd')
+        } else if (err.name==='OddError') {
+        console.error(err.message)
         } else {
         console.error('Unknown error', err)
         }
