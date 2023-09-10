fc813eddf51add5f7fa14f197f81d5f8b3fb51fd
kodrr <ntlauigan@gmail.com>
2023-09-11 03:58:20 +0800

    Edit events

--- chapter-9-Events/once.js
+++ chapter-9-Events/once.js
@@ -1,10 +1,19 @@
-const { EventEmitter } = require('events')
-const myEmitter = new EventEmitter()
-myEmitter.on('something', ()=>{console.log('emit something')})
-myEmitter.emit('something')
+const { EventEmitter } = require("events");
+const myEmitter = new EventEmitter();
+// myEmitter.on('something', ()=>{console.log('emit something')})
+// myEmitter.emit('something')
 
-myEmitter.once('once', function(){console.log('only emitted once')})
-myEmitter.emit('once')
-myEmitter.emit('once')
+// myEmitter.once('once', function(){console.log('only emitted once')})
+// myEmitter.emit('once')
+// myEmitter.emit('once')
 
+function run(ee) {
+  ee.once("event", () => {
+    console.log("run me only once");
+  });
+}
 
+run(myEmitter);
+myEmitter.emit("event");
+myEmitter.emit("event");
+myEmitter.emit("event");
--- chapter-9-Events/prepend.js
+++ chapter-9-Events/prepend.js
@@ -1,6 +1,15 @@
-const { EventEmitter } = require('events')
-const ee = new EventEmitter()
+const { EventEmitter } = require("events");
+const ee = new EventEmitter();
 
-ee.on('my-event', ()=>{console.log('2nd event')})
-ee.prependListener('my-event', ()=>{console.log('1st event')})
-ee.emit('my-event')
+ee.on("my-event", () => {
+  console.log("2nd event");
+});
+
+ee.prependListener("my-event", () => {
+  console.log("1st event");
+}); //prepend in every emit of 'my-event'
+//prependOnceListener prepend just once in many emit of 'my-event'
+
+ee.emit("my-event");
+ee.emit("my-event");
+ee.emit("my-event");
